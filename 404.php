45e8c60afeba40f4806ddbc8644899e9ffc420a8
Josh Betz <betzster@gmail.com>
2012-04-18 16:57:08 -0500

    fix 404 after testing

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div id="main" role="main">
 
   <article id="post-<?php the_ID(); ?>">
-    <h1>Error: 404(1)</h1>
+    <h1>Error: 404</h1>
     <section class="entry">
       <p>So, this is embarasing. The page you're looking for doesn't seem to be here. Try searching for it? I don't know what else to say.</p>
     </section>

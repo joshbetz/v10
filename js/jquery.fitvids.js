c9444dbc129db1a4f56990285075e4e80c071736
Josh Betz <betzster@gmail.com>
2012-05-18 09:55:20 -0500

    update fitvids to latest

--- js/jquery.fitvids.js
+++ js/jquery.fitvids.js
@@ -19,7 +19,7 @@
     var div = document.createElement('div'),
         ref = document.getElementsByTagName('base')[0] || document.getElementsByTagName('script')[0];
         
-  	div.className = 'fit-vids-style';
+    div.className = 'fit-vids-style';
     div.innerHTML = '&shy;<style>         \
       .fluid-width-video-wrapper {        \
          width: 100%;                     \
@@ -46,11 +46,10 @@
     
     return this.each(function(){
       var selectors = [
-        "iframe[src^='http://player.vimeo.com']", 
-        "iframe[src^='http://www.youtube.com']", 
-        "iframe[src^='https://www.youtube.com']", 
-        "iframe[src^='http://www.kickstarter.com']", 
-        "iframe[src^='//speakerdeck.com']", 
+        "iframe[src*='player.vimeo.com']", 
+        "iframe[src*='www.youtube.com']",  
+        "iframe[src*='www.kickstarter.com']", 
+        "iframe[src*='speakerdeck.com']", 
         "object", 
         "embed"
       ];
@@ -66,10 +65,10 @@
         if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; } 
         var height = this.tagName.toLowerCase() == 'object' ? $this.attr('height') : $this.height(),
             aspectRatio = height / $this.width();
-		if(!$this.attr('id')){
-			var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-			$this.attr('id', videoID);
-		}
+    if(!$this.attr('id')){
+      var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+      $this.attr('id', videoID);
+    }
         $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
         $this.removeAttr('height').removeAttr('width');
       });
